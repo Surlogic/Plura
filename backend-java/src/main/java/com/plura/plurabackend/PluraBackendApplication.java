483f33bd2bb3c00dfdc96ad8689950e8dd081c59
Surlogicuy <admin@surlogicuy.com>
2026-02-22 19:26:53 -0300

    cambiando el backend a java con spring boot

--- backend-java/src/main/java/com/plura/plurabackend/PluraBackendApplication.java
+++ backend-java/src/main/java/com/plura/plurabackend/PluraBackendApplication.java
@@ -2,11 +2,14 @@ package com.plura.plurabackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class PluraBackendApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 		SpringApplication.run(PluraBackendApplication.class, args);
 	}
 

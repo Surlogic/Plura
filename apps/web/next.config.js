f1e0224cea19f284752442e84dd2d0dd4e3c8a16
Surlogicuy <admin@surlogicuy.com>
2026-03-14 20:29:45 -0300

    arreglando para que levante en local

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -70,6 +70,7 @@ const nextConfig = {
       ...extraImageRemoteHosts,
     ],
   },
+  turbopack: {},
   webpack: (config) => {
     config.resolve.alias = {
       ...(config.resolve.alias || {}),
